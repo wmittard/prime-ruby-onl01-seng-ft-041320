cb8decfa4556ee50c58de8efa314358b55c96b31
William Mittard <billymittard@ymail.com>
2020-04-15 23:16:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(number)
   start = 2 
   if number > 1 
-    range = (start..number-).to_a
+    range = (start..number-1).to_a
     range.none? |num test|
     
     number % num_to_test == 0 
